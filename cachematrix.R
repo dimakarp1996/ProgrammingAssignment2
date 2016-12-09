7f05bf88f8d5a3c40248e06f05c9f13b056ac5d7
dimakarp1996 <dimakarp1996@yandex.ru>
2016-12-09 23:14:36 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -6,27 +6,30 @@ makeCacheMatrix <- function(x=matrix())
     print("Error")
     warning()
   }
-  set<-function(y)
+  set<-function(y)#here we set a variable
   {
     x<<-y
     m<<-NULL
   }
-  toCache<-function(obj)
+  toCache<-function(obj)#here we move solution to the cache
   {m<<-obj}
-  getx<-function(){x}
-  getInvert<-function(){m}
-  list(set=set,toCache=toCache,getx=getx,getInvert=getInvert)
+  getx<-function(){x}#here we read a variable
+  getInvert<-function(){m}#here we read a solution from the cache
+  list(set=set,toCache=toCache,getx=getx,getInvert=getInvert)#function returns a list of methods
 }
 cacheSolve<-function(y,...)
 {
-  m<-y$getInvert()
+  m<-y$getInvert()#we check if the matrix is already inverted
   if(!is.null(m))
   {
     print("Data are cached")
-    m
+    m#if yes - we do nothing
   }
-  data<-y$getx()
-  m<-solve(data)
-  y$toCache(m)
-  m
+  else
+  {
+	  data<-y$getx()#if no - we get variable
+	  m<-solve(data)#invert
+	  y$toCache(m)#cache
+	  m#and return
+  }
 }
